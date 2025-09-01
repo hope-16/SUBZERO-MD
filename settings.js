2283aa2a1e06ee0511626b832eeeadaacaecf44d
hope-16 <reaganssemwanga98@gmail.com>
2025-09-01 14:32:12 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;SJ0kDBrS#uH3y0sR2FQIYiGkRcmbKiMZpqBZZ9IS-Xlx8ixeV-tk", // Put Session Id Here Inside "*"
+  SESSION_ID:, // Put Session Id Here Inside "SUBZERO-MD;;;SJ0kDBrS#uH3y0sR2FQIYiGkRcmbKiMZpqBZZ9IS-Xlx8ixeV-tk"
   OWNER_NUMBER: "256747244950", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
